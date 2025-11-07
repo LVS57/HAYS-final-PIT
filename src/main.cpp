cc0873ccc9d96846ddb99acf53f0af45f19f0d20
IyanuKwent <olandria.iankent@gmail.com>
2025-11-08 01:03:57 +0800

    Refactor RFID handling and WiFi connection logic; update server endpoint and improve data sending process

--- src/main.cpp
+++ src/main.cpp
@@ -5,73 +5,89 @@
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
 
+
 #define SS_PIN 5
 #define RST_PIN 0
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 WiFiMulti wifiMulti;
 
-const char* serverScan = "http://10.10.10.33/insert.php";
+
+const char* serverName = "http://10.10.10.33/esp32/insert.php";
+
 
 void readRFID();
-void sendScan(const String& rfidTag);
+void sendRFIDData(String rfidData);
 void connectToWiFi();
 
-String lastTag = "";
+
+String rfidData = "";
 unsigned long lastReadTime = 0;
-const unsigned long readInterval = 1500; 
+const unsigned long readInterval = 2000;
 bool wifiConnected = false;
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
 
+  
   SPI.begin(18, 19, 23);
   rfid.PCD_Init();
   Serial.println("RFID reader initialized");
 
+  // Set WiFi mode and add networks
   WiFi.mode(WIFI_STA);
   wifiMulti.addAP("Cloud Control Network", "ccv7network");
-
+  wifiMulti.addAP("Cloud Control Network", "ccv7network");
+  
   connectToWiFi();
+  
   Serial.println("System ready. Scan RFID cards...");
+  Serial.println();
 }
 
 void connectToWiFi() {
   int attempts = 0;
-  while (wifiMulti.run() != WL_CONNECTED && attempts < 30) {
-    Serial.print('.');
+  while (wifiMulti.run() != WL_CONNECTED && attempts < 15) {
+    Serial.print(".");
     delay(500);
     attempts++;
   }
+  
   if (wifiMulti.run() == WL_CONNECTED) {
     wifiConnected = true;
-    Serial.println("\nWiFi connected");
-    Serial.print("IP: "); Serial.println(WiFi.localIP());
+    Serial.println("\nWiFi connected successfully!");
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
   } else {
-    wifiConnected = false;
-    Serial.println("\nWiFi not connected");
+    Serial.println("\nFailed to connect to WiFi");
+    Serial.println();
   }
 }
 
 void loop() {
-  if (wifiMulti.run() != WL_CONNECTED) {
+  if (wifiMulti.run() == WL_CONNECTED) {
+    if (!wifiConnected) {
+      wifiConnected = true;
+      Serial.println("WiFi reconnected!");
+    }
+    
+    if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+      readRFID();
+    }
+  } else {
     if (wifiConnected) {
       wifiConnected = false;
-      Serial.println("WiFi disconnected");
+      Serial.println("WiFi disconnected!");
     }
     delay(2000);
     connectToWiFi();
-    return;
-  } else if (!wifiConnected) {
-    wifiConnected = true;
-    Serial.println("WiFi reconnected");
   }
-
-  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
-    readRFID();
-  }
-  delay(30);
+  
+  delay(50);
 }
 
 void readRFID() {
@@ -80,58 +96,56 @@ void readRFID() {
     rfid.PCD_StopCrypto1();
     return;
   }
-
-  String tag = "";
-  for (byte i = 0; i < rfid.uid.size; i++) {
-    tag += String(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
-    tag += String(rfid.uid.uidByte[i], HEX);
-  }
-  tag.toUpperCase();
-
   
-  lastReadTime = millis();
-  if (tag == lastTag) {
-    rfid.PICC_HaltA();
-    rfid.PCD_StopCrypto1();
-    return;
+  rfidData = "";
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    rfidData.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : ""));
+    rfidData.concat(String(rfid.uid.uidByte[i], HEX));
   }
-  lastTag = tag;
-
+  rfidData.toUpperCase();
+  
+  Serial.println();
   Serial.print("RFID Detected: ");
-  Serial.println(tag);
-
-  sendScan(tag);
-
+  Serial.println(rfidData);
+  
+  sendRFIDData(rfidData);
+  
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
+  lastReadTime = millis();
 }
 
-void sendScan(const String& rfidTag) {
+void sendRFIDData(String rfidData) {
   if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Cannot send scan - WiFi disconnected");
+    Serial.println("Cannot send data - WiFi disconnected");
     return;
   }
 
   WiFiClient client;
   HTTPClient http;
 
-  Serial.print("POST "); Serial.println(serverScan);
-  if (!http.begin(client, serverScan)) {
-    Serial.println("HTTP begin failed");
-    return;
-  }
+  if (!http.begin(client, serverName)) return;
 
   http.addHeader("Content-Type", "application/json");
   http.setTimeout(10000);
 
-  String payload = String("{\"rfid_data\":\"") + rfidTag + "\"}";
-  int code = http.POST(payload);
-  if (code > 0) {
-    Serial.print("HTTP "); Serial.println(code);
-    String resp = http.getString();
-    Serial.println(resp);
+  String jsonPayload = "{\"rfid_data\":\"" + rfidData + "\"}";
+  Serial.println("Attempting to send data to server...");
+  Serial.print("JSON Payload: ");
+  Serial.println(jsonPayload);
+  
+  int httpResponseCode = http.POST(jsonPayload);
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String response = http.getString();
+    Serial.print("Server response: ");
+    Serial.println(response);
+    Serial.println();
   } else {
-    Serial.print("HTTP error: "); Serial.println(code);
+    Serial.print("Error sending data. Code: ");
+    Serial.println(httpResponseCode);
   }
+  
   http.end();
 }
